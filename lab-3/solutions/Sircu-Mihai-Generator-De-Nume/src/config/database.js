97a923f513bf1817c6ba3af2b6f753254bee331f
SircuMihai <mihailsircu5@gmail.com>
2025-11-21 18:50:19 +0200

    ans a littel bit more

--- lab-3/solutions/Sircu-Mihai-Generator-De-Nume/src/config/database.js
+++ lab-3/solutions/Sircu-Mihai-Generator-De-Nume/src/config/database.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, '..\', '..\', 'database.sqlite'),
+  storage: path.resolve(__dirname, '..', '..', 'database.sqlite'),
   logging: false, // Set to true to see SQL queries in console
 });
 
